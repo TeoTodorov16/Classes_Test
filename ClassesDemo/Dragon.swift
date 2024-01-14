dffcbaa2ccf4cc705f3500b257a1f2ae84b92fab
Teodor <teodorttodorov@icloud.com>
2024-01-14 21:44:28 +0200

    first commit

---
+++ ClassesDemo/Dragon.swift
@@ -0,0 +1,8 @@
+//
+//  Dragon.swift
+//  ClassesDemo
+//
+//  Created by Teo on 14.01.24.
+//
+
+import Foundation
---
+++ ClassesDemo/Enemy.swift
@@ -0,0 +1,8 @@
+//
+//  Enemy.swift
+//  ClassesDemo
+//
+//  Created by Teo on 12.01.24.
+//
+
+import Foundation
